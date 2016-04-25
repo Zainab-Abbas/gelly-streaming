3a0ba7682c2ef6a2ca6dacf54f5afe85051a8e92
Zainab Abbas <zainababbas25391@gmail.com>
2016-04-25 17:52:24 +0200

    use of keyselector and custom partitioner

--- src/main/java/org/apache/flink/graph/streaming/partitioner/GreedyLinear.java
+++ src/main/java/org/apache/flink/graph/streaming/partitioner/GreedyLinear.java
@@ -4,4 +4,5 @@ package org.apache.flink.graph.streaming.partitioner;
  * Created by zainababbas on 18/04/16.
  */
 public class GreedyLinear {
+	//TODO: Implement linear greedy from stanon & kaliot dude
 }
